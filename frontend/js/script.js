943a71f6d7a340a827731317b0fdd4f826ffbe54
Leonardo <leonardobutne78@gmail.com>
2024-09-09 22:45:27 +0200

    comWSS

--- frontend/js/script.js
+++ frontend/js/script.js
@@ -82,7 +82,7 @@ const handleLogin = (event) => {
     login.style.display = "none"
     chat.style.display = "flex"
 
-    websocket = new WebSocket("wss://chat-front-kappa.vercel.app/")
+    websocket = new WebSocket("wss://free-chat-front.onrender.com")
     websocket.onopen = () => websocket.send(`Usuario: ${user.name} entrou no chat`)
     
     websocket.onmessage = processMessage
