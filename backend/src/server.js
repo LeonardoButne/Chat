2eb15ebc1b280f3cad6aa091eadf75cd595d78da
Leonardo <leonardobutne78@gmail.com>
2024-09-09 22:16:35 +0200

    fim

--- backend/src/server.js
+++ backend/src/server.js
@@ -8,9 +8,10 @@ const wss = new WebSocketServer ({port: process.env.PORT || 8888})
 wss.on("connection", (ws) => {
 
     ws.on("erro", console.error)
+    ws.send("Mensagem enviada pelo servidor")
 
     ws.on("message", (data) => {
-
+        console.log(data.toString())
         wss.clients.forEach((client) => client.send(data.toString()))
         
     })
