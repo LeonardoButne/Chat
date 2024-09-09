dde794d1ba74080116fa82b53524435fbb91595d
Leonardo <leonardobutne78@gmail.com>
2024-09-09 22:26:08 +0200

    comDeploy

--- frontend/js/script.js
+++ frontend/js/script.js
@@ -82,7 +82,7 @@ const handleLogin = (event) => {
     login.style.display = "none"
     chat.style.display = "flex"
 
-    websocket = new WebSocket("ws://localhost:8080")
+    websocket = new WebSocket("ws://free-chat-zhvl.onrender.com")
     websocket.onopen = () => websocket.send(`Usuario: ${user.name} entrou no chat`)
     
     websocket.onmessage = processMessage
